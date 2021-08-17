18e63c37f44ff9c6478dfa4ed8c7060303025c5a
Kadir Atesoglu <atesoglu@live.com>
2021-08-17 03:25:38 +0300

    "Remove lines containing" implementation.

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, Fragment, useState } from 'react';
-import { Button, Checkbox } from 'antd';
+import { Button, Checkbox, Input } from 'antd';
 //import { Input } from 'antd';
 
 //const { TextArea } = Input;
@@ -9,6 +9,7 @@ import { CheckboxChangeEvent } from 'antd/lib/checkbox';
 
 export default function TextEditor() {
   const [content, setContent] = useState('');
+  const [linesSearchKeyword, setLinesSearchKeyword] = useState('');
   const [isRemoveDuplicateLinesCaseSensitive, setRemoveDuplicateLinesCaseSensitive] = useState(false);
 
   function onContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
@@ -22,7 +23,7 @@ export default function TextEditor() {
     var hashKey = '';
     var contentOutArray = [];
     var contentOutArrayLength = 0;
-    if (isRemoveDuplicateLinesCaseSensitive === true) {
+    if (isRemoveDuplicateLinesCaseSensitive) {
       for (var i = 0; i < contentArrayLength; i++) {
         hashKey = contentArray[i];
         if (hash[hashKey] == null) {
@@ -62,6 +63,38 @@ export default function TextEditor() {
     setContent(contentOutArray.join('\n'));
   }
 
+  function onRemoveLinesContaining(event: React.MouseEvent) {
+    var contentArray = content.replace(/\r/g, '').split('\n');
+    var contentArrayLength = contentArray.length;
+    var contentOutArray = [];
+    var contentOutArrayLength = 0;
+    var testRegex = new RegExp(/\S/);
+    for (var x = 0; x < contentArrayLength; x++) {
+      if (testRegex.test(contentArray[x])) {
+        contentOutArray[contentOutArrayLength] = contentArray[x];
+        contentOutArrayLength++;
+      }
+    }
+    setContent(contentOutArray.join('\n'));
+  }
+  function onRemoveLinesNotContaining(event: React.MouseEvent) {
+    var contentArray = content.replace(/\r/g, '').split('\n');
+    var contentArrayLength = contentArray.length;
+    var contentOutArray = [];
+    var contentOutArrayLength = 0;
+    var testRegex = new RegExp(/\S/);
+    for (var x = 0; x < contentArrayLength; x++) {
+      if (testRegex.test(contentArray[x])) {
+        contentOutArray[contentOutArrayLength] = contentArray[x];
+        contentOutArrayLength++;
+      }
+    }
+    setContent(contentOutArray.join('\n'));
+  }
+  function onRemoveLinesKeywordChange(event: ChangeEvent<HTMLInputElement>) {
+    setLinesSearchKeyword(event.target.value);
+  }
+
   return (
     <Fragment>
       <div className="flex h-full">
@@ -70,18 +103,38 @@ export default function TextEditor() {
         </div>
         <aside className="flex-shrink w-96 focus:outline-none bg-gray-100 px-2 py-1 border-r border-gray-200">
           <div className="flex flex-col space-y-3">
-            <div className="flex">
-              <Button type="primary" onClick={onRemoveDuplicateLines} className="flex-grow">
-                <FontAwesomeIcon icon={faEraser} className="text-white" /> Remove Duplicate Lines
-              </Button>
+            <div>
+              <h3>Remove Duplicate Lines</h3>
+              <div className="flex">
+                <Button type="primary" onClick={onRemoveDuplicateLines} className="flex-grow">
+                  <FontAwesomeIcon icon={faEraser} className="text-white" /> Remove
+                </Button>
 
-              <Checkbox checked={isRemoveDuplicateLinesCaseSensitive} onChange={onRemoveDuplicateLinesCaseSensitiveChange} className="flex-shrink pl-2 pt-2">
-                Case-sensitive
-              </Checkbox>
+                <Checkbox checked={isRemoveDuplicateLinesCaseSensitive} onChange={onRemoveDuplicateLinesCaseSensitiveChange} className="flex-shrink pl-2 pt-2">
+                  Case-sensitive
+                </Checkbox>
+              </div>
             </div>
-            <Button type="primary" onClick={onRemoveEmptyLines}>
-              <FontAwesomeIcon icon={faEraser} className="text-white" /> Remove Empty Lines
-            </Button>
+
+            <div>
+              <h3>Remove Empty Lines</h3>
+              <Button type="primary" onClick={onRemoveEmptyLines}>
+                <FontAwesomeIcon icon={faEraser} className="text-white" /> Remove Empty Lines
+              </Button>
+            </div>
+
+            <div>
+              <h3>Remove Lines...</h3>
+              <div className="flex space-x-1">
+                <Input placeholder="Search keyword" value={linesSearchKeyword} onChange={onRemoveLinesKeywordChange} />
+                <Button type="primary" onClick={onRemoveLinesContaining} className="flex-grow">
+                  <FontAwesomeIcon icon={faEraser} className="text-white" /> Containing
+                </Button>
+                <Button type="primary" onClick={onRemoveLinesNotContaining} className="flex-grow">
+                  <FontAwesomeIcon icon={faEraser} className="text-white" /> Not Containing
+                </Button>
+              </div>
+            </div>
           </div>
         </aside>
       </div>
